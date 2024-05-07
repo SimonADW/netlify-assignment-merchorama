485980d94dd205e065de2155d8eed4b59d94809c
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-05-07 23:12:10 +0200

    Add js to home links and menu

---
+++ src/js/app.js
@@ -0,0 +1,62 @@
+const homeMainSection = document.querySelector(".home__main-section");
+
+// DISPLAY INDEX MENU
+const homeMenuButton = document.querySelector(".header__menu-button");
+const homeMenuDrawer = document.querySelector(".menudrawer");
+const homeDrawerCloseButton = document.querySelector(".menudrawer__close");
+
+homeMenuButton.addEventListener("click", ()=> {
+	homeMenuDrawer.style.display = "flex";
+})
+
+homeDrawerCloseButton.addEventListener("click", ()=> {
+	homeMenuDrawer.style.display = "none";
+})
+
+// DISPLAY SIGN IN FORM
+const signInSection = document.querySelector(".sign-in");
+const signInSectionClose = document.querySelector(".sign-in__close");
+const homeSignInLink = document.querySelector(".home-sign-in-link");
+const menuSignInLink = document.querySelector(".menu-sign-in-link");
+
+homeSignInLink.addEventListener("click", ()=>{
+	signInSection.style.display = "flex";	
+	homeMainSection.style.display = "none";
+})
+
+menuSignInLink.addEventListener("click", ()=>{
+	signInSection.style.display = "flex";	
+	homeMainSection.style.display = "none";
+	homeMenuDrawer.style.display = "none";
+	signUpSection.style.display = "none";	
+
+})
+
+signInSectionClose.addEventListener("click", ()=> {
+	signInSection.style.display = "none";	
+	homeMainSection.style.display = "flex";
+})
+
+// DISPLAY SIGN UP FORM - CREATE ACCOUNT FORM
+const signUpSection = document.querySelector(".sign-up");
+const signUpSectionClose = document.querySelector(".sign-up__close");
+const homeSignUpLink = document.querySelector(".home-sign-up-link");
+const menuSignUpLink = document.querySelector(".menu-sign-up-link");
+
+homeSignUpLink.addEventListener("click", ()=>{
+	signUpSection.style.display = "flex";	
+	homeMainSection.style.display = "none";
+})
+
+menuSignUpLink.addEventListener("click", ()=>{
+	signUpSection.style.display = "flex";	
+	homeMainSection.style.display = "none";
+	homeMenuDrawer.style.display = "none";
+	signInSection.style.display = "none";	
+})
+
+signUpSectionClose.addEventListener("click", ()=> {
+	signUpSection.style.display = "none";	
+	homeMainSection.style.display = "flex";	
+})
+
